2d5310e2212a0e2650d287e4b88a8f4f794f7f86
Mads Bondo Dydensborg <mads@dydensborg.dk>
2018-02-03 15:28:25 +0100

    Most of the infrastructure to transport the cutoff date in place

--- www/data.php
+++ www/data.php
@@ -4,16 +4,21 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
-# Move from database to json format.
-# Target format
-# [ { name: , reg_time: , count: }, ... ]
+// Move from database to json format.
+// Target format
+// [ { name: , reg_time: , count: }, ... ] - that is, a raw dump from the database.
 
 // phpinfo(INFO_MODULES);
-
 require_once('./dbconnect.php');
 
-$sql = "SELECT name, reg_time, count FROM data where reg_time > '2018-02-03 12:00:00' order by name, reg_time";
+// Parameters
+// cutoff = last value we know of, only pass newer than this.
+$cutoff = isset ($_GET['cutoff'])
+    ? $connection->real_escape_string(trim($_GET['cutoff']))
+    : "2018-01-01T12:00:00.000Z";
+
+
+$sql = "SELECT name, reg_time, count FROM data where reg_time > '$cutoff' order by name, reg_time";
 $results = $connection->query($sql);
 
 $rows = array();
--- www/scripts.js
+++ www/scripts.js
@@ -12,8 +12,36 @@ function updateData(chart) {
 
 // Can we make XMLHttpRequest?
 function getNewData() {
+    // Get the latest date from the chart data1 series, and fetch from that point
+    /* This is formatted like this:
+    
+    cutoff: [ {"id":"Støtter",
+	       "id_org":"Støtter",
+	       "values":[ {"x":"2018-02-01T14:09:59.000Z",
+			   "value":650,
+			   "id":"Støtter",
+			   "index":0},
+			  {"x":"2018-02-01T14:14:59.000Z",
+			   "value":692,
+			   "id":"Støtter",
+			   "index":1},
+			  ....
+     */
+    dataArray = chart.data('Støtter')[0].values;
+    if ( dataArray.length > 0 ) {
+	cutoff = dataArray[dataArray.length-1].x;
+    } else {
+	// Before the first borgerforslag... 
+	cutoff = new Date ( 2018, 01, 01, 12, 0, 0, 0 );
+    }
+    // alert("cutoff: " + JSON.stringify(cutoff));
+    getNewDataCutoff(cutoff);
+}
+
+// cutoff is a Date object.
+function getNewDataCutoff(cutoff) {
     var xmlhttp = new XMLHttpRequest();
-    var url = "data.php";
+    var url = "data.php?cutoff=" + encodeURI(cutoff.toISOString());
 
     xmlhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
